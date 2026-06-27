f472ade274112e970438fb43af1fc57d0a338ada
yazid-datapex <yazid@mydatapex.com>
2026-06-28 03:11:20 +0800

    Add digital identity action icon

--- components/LucideIcons.tsx
+++ components/LucideIcons.tsx
@@ -51,3 +51,12 @@ export function ContactRoundIcon(props: IconProps) {
     </IconBase>
   );
 }
+
+export function CompassIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <path d="m16.24 7.76-2.12 6.36-6.36 2.12 2.12-6.36z" />
+    </IconBase>
+  );
+}
